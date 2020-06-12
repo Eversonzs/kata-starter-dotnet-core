771e39d79c460cc3b7b81b85d4172e75b2fe275b
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-06-12 14:53:48 -0600

    function when user input is unknown numbers

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -11,6 +11,13 @@ namespace Kata
                 return 0;                
             }
             
+            if (number.Split(",").Length > 1)
+            {
+                int number1 = Int32.Parse(number.Split(",")[0]);
+                int number2 = Int32.Parse(number.Split(",")[1]);
+                return number1 + number2;
+            }
+            
             return Int32.Parse(number);
         }
     }
